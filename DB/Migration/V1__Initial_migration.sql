f6c3fe2f94ac1e766bca659e2daab66323340f8e
santoshashtagi1983 <s.ashtagi1983@gmail.com>
2025-01-31 21:04:34 +0530

    yml file

---
+++ DB/Migration/V1__Initial_migration.sql
@@ -0,0 +1,15 @@
+-- Use the newly created database
+USE MyDatabase;
+
+-- Create a table named 'Users' within 'MyDatabase'
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    UserName VARCHAR(100),
+    UserEmail VARCHAR(100),
+    UserPassword VARCHAR(100)
+);
+
+-- Insert sample data into the 'Users' table
+INSERT INTO Users (UserID, UserName, UserEmail, UserPassword)
+VALUES (1, 'John Doe', 'john.doe@example.com', 'password123'),
+       (2, 'Jane Smith', 'jane.smith@example.com', 'password456');
